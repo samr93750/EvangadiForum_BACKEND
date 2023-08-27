b6609e68b9755062dc4a153771cf4ed11fbd1683
samr93750 <amaresamr93@gmail.com>
2023-08-27 14:12:42 +0300

    commit

--- server.js
+++ server.js
@@ -11,17 +11,28 @@ const port = process.env.PORT||80;
 app.use(express.json());
 // app.use(cors());
 const allowedOrigins = ["https://forumfrontend-samrawit.onrender.com"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Expose-Headers", "Authorization"); // Expose any additional headers if needed
+  next();
+});
+app.use(cors)
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+
 app.use(express.urlencoded({ extended: true }));
  
 app.use("/api/users", userRouter);
